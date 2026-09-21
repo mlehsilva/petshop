50c36846dcb7f47f9d4d82170926fc2de2bbc199
mlehsilva <marialeticiadasilva0116@gmail.com>
2026-09-21 18:06:29 -0300

    ´poiugtf

--- backEnd/db.js
+++ backEnd/db.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2');
 
 /* cria um pool de conexoes configurado para a porta padrao 3306 */
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: process.env.DB_HOST || 'localhost', /* Usa 'db' no Docker ou 'localhost' na máquina */
   port: 3306,
-  user: 'root',          /* altere para o seu usuario se necessario */
-  password: 'suasenha',   /* altere para a senha do seu banco de dados */
-  database: 'oliva_co',   /* nome do banco de dados que criaremos */
+  user: 'root',          
+  password: 'suasenha',   
+  database: 'oliva_co',   
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
